c2b396e2c36c6b248787eae0e79538d83d9f17da
EC2 Default User <ec2-user@ip-172-31-27-43.ap-northeast-1.compute.internal>
2023-04-08 00:35:31 +0000

    C5 タスク一覧

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  before_action :set_user
+  
+  def index
+    @tasks = @user.tasks
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
