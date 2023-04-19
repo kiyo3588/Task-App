c9a2fbca5d7f4731af0523a4b319029ab3c01937
EC2 Default User <ec2-user@ip-172-31-27-43.ap-northeast-1.compute.internal>
2023-04-19 00:56:03 +0000

    D7タスクサンプル

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 
-User.create!(name: "管理者",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password",
-             admin: true)
+User.create!( name: "管理者",
+              email: "sample@email.com",
+              password: "password",
+              password_confirmation: "password",
+              admin: true)
                
 99.times do |n|
-  name = Faker::Name.name
+  name  = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
   User.create!(name: name,
@@ -15,3 +15,17 @@ User.create!(name: "管理者",
                password: password,
                password_confirmation: password)
 end
+
+puts "Users Created"
+
+admin_user = User.first
+guest_user = User.find(2)
+
+50.times do |n|
+  task_name = "タスク#{n + 1}"
+  description = "タスク詳細#{n + 1}"
+  admin_user.tasks.create!(name: task_name, description: description)
+  guest_user.tasks.create!(name: task_name, description: description)
+end
+
+puts "Tasks Created"
