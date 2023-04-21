fa5267ed3b25ee19eee2e8081f5f96d492397a41
EC2 Default User <ec2-user@ip-172-31-27-43.ap-northeast-1.compute.internal>
2023-04-21 00:36:09 +0000

    ヘルパーに定義追加c_u?(user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :admin_user, only: [:index, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_or_correct_user, only: :show
   
   def index
     @users = User.paginate(page: params[:page], per_page: 20)
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = '新規作成に成功しました。'
+      flash[:success] = 'ユーザーの新規作成に成功しました。'
       redirect_to @user
     else
       render :new
@@ -48,7 +49,7 @@ class UsersController < ApplicationController
       redirect_to users_url
     else
       @user.destroy
-      flash[:success] = "#{@user.name}のデーターを削除しました。"
+      flash[:success] = "#{@user.name}のデータを削除しました。"
       redirect_to users_url
     end
   end
@@ -63,4 +64,11 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
     
+    def admin_or_correct_user
+      @user = User.find(params[:id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "権限がありません。"
+        redirect_to(root_url)
+      end
+    end
 end
