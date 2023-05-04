70d6be55c37ea118d3f5621b7fa8cc3462e8564f
EC2 Default User <ec2-user@ip-172-31-27-43.ap-northeast-1.compute.internal>
2023-05-04 16:04:33 +0000

    G31 タスク一覧に戻る

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   def set_task
     unless @task = @user.tasks.find_by(id: params[:id])
       flash[:danger] = "権限がありません。"
-      redirect_to user_tasks_url(@user) 
+      redirect_to user_tasks_path(current_user)
    end
   end
 end
